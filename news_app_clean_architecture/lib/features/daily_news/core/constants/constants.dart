086b5bd0d2598e10a2e39e304dc9317731df9513
Cutie-Din <quanghung21hb@gmail.com>
2025-01-06 17:27:59 +0700

    news_clean_architecture

---
+++ news_app_clean_architecture/lib/features/daily_news/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'bc33e4d0900443ce93eea684005bf3bd';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
