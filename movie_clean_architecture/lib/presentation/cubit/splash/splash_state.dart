0b65b448a878d47f75210def181f1203a8506b0e
Cutie-Din <quanghung21hb@gmail.com>
2025-01-17 17:00:18 +0700

    movie_clean_structure

---
+++ movie_clean_architecture/lib/presentation/cubit/splash/splash_state.dart
@@ -0,0 +1,9 @@
+part of 'splash_cubit.dart';
+
+abstract class SplashState {}
+
+class Splashed extends SplashState {}
+
+class Authenticated extends SplashState {}
+
+class UnAuthenticated extends SplashState {}
